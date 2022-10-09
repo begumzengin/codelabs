392b65bb8a8b4a3bbb07f8dbb393b68713839298
Begüm Zengin <46397735+begumzengin@users.noreply.github.com>
2022-10-09 14:46:38 +0300

    completed step 6

--- boring_to_beautiful/step_01/lib/src/shared/views/adaptive_navigation.dart
+++ boring_to_beautiful/step_01/lib/src/shared/views/adaptive_navigation.dart
@@ -19,29 +19,39 @@ class AdaptiveNavigation extends StatelessWidget {
     return LayoutBuilder(
       builder: (context, dimens) {
         // Tablet Layout
-        // Add maxWidth constraint check
-        return Scaffold(
-          body: Row(
-            children: [
-              NavigationRail(
-                extended: dimens.maxWidth >= 800,
-                minExtendedWidth: 180,
-                destinations: destinations
-                    .map((e) => NavigationRailDestination(
-                          icon: e.icon,
-                          label: Text(e.label),
-                        ))
-                    .toList(),
-                selectedIndex: selectedIndex,
-                onDestinationSelected: onDestinationSelected,
-              ),
-              Expanded(child: child),
-            ],
-          ),
-        );
+        if (dimens.maxWidth >= 600) {
+          // Add maxWidth constraint check
+          return Scaffold(
+            body: Row(
+              children: [
+                NavigationRail(
+                  extended: dimens.maxWidth >= 800,
+                  minExtendedWidth: 180,
+                  destinations: destinations
+                      .map((e) => NavigationRailDestination(
+                            icon: e.icon,
+                            label: Text(e.label),
+                          ))
+                      .toList(),
+                  selectedIndex: selectedIndex,
+                  onDestinationSelected: onDestinationSelected,
+                ),
+                Expanded(child: child),
+              ],
+            ),
+          );
+        }
         // Add closing curly bracket
 
         // Add return for mobile layout
+        return Scaffold(
+          body: child,
+          bottomNavigationBar: NavigationBar(
+            destinations: destinations,
+            selectedIndex: selectedIndex,
+            onDestinationSelected: onDestinationSelected,
+          ),
+        );
       },
     );
   }
