eada217dc011833dcbdb776b48d2ce73a9cc5f2b
Begüm Zengin <46397735+begumzengin@users.noreply.github.com>
2022-10-09 14:57:30 +0300

    completed step 7

--- boring_to_beautiful/step_01/lib/src/features/playlists/view/playlist_songs.dart
+++ boring_to_beautiful/step_01/lib/src/features/playlists/view/playlist_songs.dart
@@ -40,14 +40,17 @@ class PlaylistSongs extends StatelessWidget {
         index: index,
         cells: [
           DataCell(
-            // Add HoverableSongPlayButton
-            Center(
+              // Add HoverableSongPlayButton
+              HoverableSongPlayButton(
+            hoverMode: HoverMode.overlay,
+            song: playlist.songs[index],
+            child: Center(
               child: Text(
                 (index + 1).toString(),
                 textAlign: TextAlign.center,
               ),
             ),
-          ),
+          )),
           DataCell(
             Row(children: [
               Padding(
